013315ded2c220e45f537ed98588a1988e3ae28f
Guilherme Gaigher netto <ggaighernt@gmail.com>
2024-10-30 12:49:48 +0000

    chore: remove comments

--- tests/modeling/test_load_data.py
+++ tests/modeling/test_load_data.py
@@ -9,15 +9,12 @@ from laughing_couscous.modeling.load_data import load_data
 @patch("laughing_couscous.modeling.load_data.train_test_split")
 def test_load_data(mock_train_test_split, mock_load_iris):
     """Test load_data function."""
-    # Given
     mock_load_iris.return_value.data = "data"
     mock_load_iris.return_value.target = "target"
     mock_train_test_split.return_value = ["X_train", "X_test", "y_train", "y_test"]
 
-    # When
     result = load_data()
 
-    # Then
     assert result == ["X_train", "X_test", "y_train", "y_test"]
     mock_load_iris.assert_called_once()
     mock_train_test_split.assert_called_once()
